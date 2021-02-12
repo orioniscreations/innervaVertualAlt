5f71caeac7a6a88bdc9314c7df55b312281c3acc
Spencer Price <spencerdprice2001@gmail.com>
2021-02-12 14:14:54 -0400

    trying to fix menu button

--- js/main.js
+++ js/main.js
@@ -7,30 +7,32 @@ $(window).scroll(function () {
     $('.main_h').removeClass('sticky');
   }*/
 });
+$(document).ready(function () {
+  // Mobile Navigation
+  $('.mobile-toggle').click(function () {
+    if ($('.main_h').hasClass('open-nav')) {
+      $('.main_h').removeClass('open-nav');
+    } else {
+      $('.main_h').addClass('open-nav');
+      console.log("Nav opened");
+    }
+  });
 
-// Mobile Navigation
-$('.mobile-toggle').click(function () {
-  if ($('.main_h').hasClass('open-nav')) {
-    $('.main_h').removeClass('open-nav');
-  } else {
-    $('.main_h').addClass('open-nav');
-  }
-});
+  $('.main_h li a').click(function () {
+    if ($('.main_h').hasClass('open-nav')) {
+      $('.navigation').removeClass('open-nav');
+      $('.main_h').removeClass('open-nav');
+    }
+  });
 
-$('.main_h li a').click(function () {
-  if ($('.main_h').hasClass('open-nav')) {
-    $('.navigation').removeClass('open-nav');
-    $('.main_h').removeClass('open-nav');
-  }
-});
-
-// navigation scroll lijepo radi materem
-$('nav a').click(function (event) {
-  var id = $(this).attr("href");
-  var offset = 70;
-  var target = $(id).offset().top - offset;
-  $('html, body').animate({
-    scrollTop: target
-  }, 500);
-  event.preventDefault();
+  // navigation scroll lijepo radi materem
+  $('nav a').click(function (event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+      scrollTop: target
+    }, 500);
+    event.preventDefault();
+  });
 });
